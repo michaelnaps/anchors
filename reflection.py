6896f45d29c4007e7c2c35f8be3f6ee292a6faf0
Michael Napoli <mikednapoli@gmail.com>
2023-08-22 21:25:18 -0400

    created global reflection set

--- reflection.py
+++ reflection.py
@@ -12,24 +12,12 @@ q = 2*A*np.random.rand( 2,1 ) - A
 
 # Anchor and reflection sets.
 aList = 2*A*np.random.rand( 2,Na ) - A
-
+rxList = np.vstack( (np.zeros( (1,Na) ), aList[1]) )
+ryList = np.vstack( (aList[0], np.zeros( (1,Na) )) )
 
 # Control matrices.
-D = 1/2*np.diag( [
-    1/np.sum( aList[0] ), 1/np.sum( aList[1] )
-] )
-Q = D@np.sum(
-    2*q*aList - aList**2, axis=1
-)[:,None]
-
-
-# Reflection set.
-def getReflectionSet( axis=0 ):
-    rxList = np.zeros( (2,Na) )
-    ryList = np.zeros( (2,Na) )
-    rxList[1,:] = aList[1]
-    ryList[0,:] = aList[0]
-    return rxList, ryList
+D = 1/2*np.diag( [ 1/np.sum( aList[0] ), 1/np.sum( aList[1] ) ] )
+Q = D@np.sum( 2*q*aList - aList**2, axis=1 )[:,None]
 
 
 # Anchor measurement functions.
@@ -39,10 +27,13 @@ def anchorMeasure(x):
         d[:,i] = (x - a[:,None]).T@(x - a[:,None])
     return np.sqrt( d )
 
-def reflectionMeasure(x, rList):
-    dr = np.empty( (1,Na) )
-    for i, r in enumerate( rList.T ):
-        dr[:,i] = (x - r[:,None]).T@(x - r[:,None])
+def reflectionMeasure(x):
+    dr = np.empty( (2,Na) )
+    for i, (rx, ry) in enumerate( zip( rxList.T, ryList.T ) ):
+        dr[:,i] = np.array( [
+            (x - rx[:,None]).T@(x - rx[:,None]),
+            (x - ry[:,None]).T@(x - ry[:,None]),
+        ] )[:,0,0]
     return np.sqrt( dr )
 
 
@@ -51,12 +42,12 @@ def anchorControl(x):
 
     # Combine anchor sets.
     dList = anchorMeasure( x )
-    rList = reflectionMeasure( x )
+    drList = reflectionMeasure( x )
 
     # Calculate measurement state.
     d = np.array( [
-        np.sum( dList**2 - rList[0]**2, axis=1 ),
-        np.sum( dList**2 - rList[1]**2, axis=1 )
+        np.sum( dList**2 - drList[0]**2, axis=1 ),
+        np.sum( dList**2 - drList[1]**2, axis=1 )
     ] )
 
     # Return control.
