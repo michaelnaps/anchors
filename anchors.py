17a5d43d7606669e66ff7520a95216d73ea606b2
Michael Napoli <mikednapoli@gmail.com>
2023-08-22 20:36:55 -0400

    initializing repo

---
+++ anchors.py
@@ -0,0 +1,14 @@
+import sys
+from os.path import expanduser
+sys.path.insert(0, expanduser('~')+'/prog/geom')
+
+import numpy as np
+from GEOM.Vehicle2D import *
+
+# Model function.
+def model(x, u):
+    return x + dt*u
+
+# Control methods.
+def control(x, q=[[0],[0]]):
+    return q - x
