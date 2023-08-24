9e1aa5a68b58d449bf622364d684498e6a67fd03
Michael Napoli <mnapoli@irobot.com>
2023-08-24 09:15:52 -0400

    enters

--- anchors.py
+++ anchors.py
@@ -5,11 +5,13 @@ sys.path.insert(0, expanduser('~')+'/prog/geom')
 import numpy as np
 from GEOM.Vehicle2D import *
 
+
 # True system values.
 A = 2
 dt = 0.01
 Nx = 2
 Nu = 2
+
 
 # Model function.
 def model(x, u):
