5a9f4f44507655b58d1d657a1f15c32b04034e59
Michael Napoli <mikednapoli@gmail.com>
2023-09-04 18:16:50 -0400

    new anchor measure function

--- symmetric/multi.py
+++ symmetric/multi.py
@@ -6,7 +6,7 @@ from root import *
 
 
 # Anchor values.
-N = 3    # Number of anchors.
+N = 2    # Number of anchors.
 M = 3*N  # Number of vehicles.
 
 # Anchor and corresponding reflection sets.
@@ -17,26 +17,20 @@ print( 'A:\n', A )
 print( 'Ax:\n', Ax )
 print( 'Ay:\n', Ay )
 
-# Anchor-distance control matrices.
-D = -1/4*np.diag( [ 1/np.sum( A[0] ), 1/np.sum( A[1] ) ] )
-print( 'Anchor coefficient matrices:\n', D )
+# # Anchor-distance control matrices.
+# D = -1/4*np.diag( [ 1/np.sum( A[0] ), 1/np.sum( A[1] ) ] )
+# print( 'Anchor coefficient matrices:\n', D )
 
 
 # Anchor measurement functions.
-def anchorMeasure(x):
+def anchorMeasure(x, aList=None):
+    if aList is None:
+        aList = A
+    Na = aList.shape[1]
     d = np.empty( (1,Na) )
-    for i, a in enumerate( A.T ):
+    for i, a in enumerate( aList.T ):
         d[:,i] = (x - a[:,None]).T@(x - a[:,None])
     return np.sqrt( d )
-
-def reflectionMeasure(x):
-    dr = np.empty( (2,Na) )
-    for i, (ax, ay) in enumerate( zip( Ax.T, Ay.T ) ):
-        dr[:,i] = np.array( [
-            (x - ax[:,None]).T@(x - ax[:,None]),
-            (x - ay[:,None]).T@(x - ay[:,None]),
-        ] )[:,0,0]
-    return np.sqrt( dr )
 
 
 # Main execution block.
@@ -51,10 +45,33 @@ if __name__ == '__main__':
 
     # Initialize control sets.
     k = 0
-    C = np.empty( (M,2,N-1) )
+    Z = np.empty( (M,2,2) )
     for i, x in enumerate( X.T ):
         if k == N:  k = 0
         Atemp = np.array( [A[:,j] for j in range( N ) if k != j] ).T
-        C[i] = -1/4*np.diag( [ 1/np.sum( Atemp[0] ), 1/np.sum( Atemp[1] ) ] )
+        Z[i] = -1/4*np.diag( [ 1/np.sum( Atemp[0] ), 1/np.sum( Atemp[1] ) ] )
         k += 1
-    print( 'C:\n', C )
+    print( 'Z:\n', Z )
+
+    # Swarm variables.
+    R = 0.25
+    fig, axs = plt.subplots()
+    swrm = Swarm2D( X, fig=fig, axs=axs,
+        radius=R, color='cornflowerblue' ).draw()
+
+    # Axis setup.
+    plt.axis( Abound*np.array( [-1, 1, -1, 1] ) )
+    plt.gca().set_aspect( 'equal', adjustable='box' )
+    plt.xticks( [-i for i in range( -Abound,Abound+1 )] )
+    plt.yticks( [-i for i in range( -Abound,Abound+1 )] )
+    plt.show( block=0 )
+
+    # Simulation block.
+    T = 10;  Nt = round( T/dt ) + 1
+    for i in range( Nt ):
+        for x in X.T:
+            D = anchorMeasure( x[:,None], aList=A )
+            Dx = anchorMeasure( x[:,None], aList=Ax )
+            Dy = anchorMeasure( x[:,None], aList=Ay )
+
+    input( "Press ENTER to exit program... " )
