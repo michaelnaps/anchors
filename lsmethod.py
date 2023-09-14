2f5e6ea8b75207506afe5ca5d21869cf35c7d3a8
Michael Napoli <mikednapoli@gmail.com>
2023-09-13 22:24:28 -0400

    ls method working for asymmetric points

--- lsmethod.py
+++ lsmethod.py
@@ -8,7 +8,9 @@ N = 3
 P = N*N
 
 # Anchor set initialization.
-A = np.array( [[-1, 1, 1],[1, 1, -1]] )
+# A = np.array( [[-1, 1, 1],[1, 1, -1]] )
+# A = np.array( [[-1, 2, 3],[4, 5, -6]] )
+A = noise( eps=Abound, shape=(n,N) )
 print( 'A:\n', A )
 
 # Anchor-based control coefficients.
@@ -23,17 +25,21 @@ print( 'Ka:\n', Ka )
 
 # Measurement stack for control synthesis.
 def anchorMeasureStack( X, A ):
-    z = anchorMeasure( X, A )
-    H = np.vstack( (
-        [ [dp**2 - dq**2 - ap@ap[:,None] + aq@ap[:,None]
-            for p, (dp, ap) in enumerate( zip( z.T, A.T ) ) if p!=q]
-                for q, (dq, aq) in enumerate( zip( z.T, A.T ) ) ]
-    ) )
-    return H
+    M = X.shape[1]
+    N = A.shape[1]
+    H = np.zeros( (N*(N-1),M) )
+    for i, x in enumerate( X.T ):
+        pq = 0
+        D = anchorMeasure( x[:,None], A )
+        for p, (dp, ap) in enumerate( zip( D.T, A.T ) ):
+            for q, (dq, aq) in enumerate( zip( D.T, A.T ) ):
+                if p != q:
+                    H[pq,i] = dp**2 - dq**2 - ap@ap[:,None] + aq@aq[:,None]
+                    pq += 1
+    return -1*H
 
 # Testing.
 X = noise( eps=Abound, shape=(Nx,N) )
 H = anchorMeasureStack( X, A )
-print( 'H:\n', H )
 print( 'X is\n', X )
 print( 'X is approximately\n', Ka@H )
