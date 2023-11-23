488358eda509ddd57a45747013836f971483d8b5
Michael Napoli <mnapoli@irobot.com>
2023-11-23 17:57:09 -0500

    homing break saved

--- single/break.py
+++ single/break.py
@@ -20,7 +20,7 @@ Aset = Abound/2*np.array( [
 Xeq = np.array( [[0],[0]] )  # noiseCirc( eps=Abound/4, N=1 )
 
 # Control formula components.
-C, K, B = distanceBasedControlMatrices( Aset, m )
+C, K, B = distanceBasedControlMatrices( rotz( np.pi/4 )@Aset, m )
 
 # Main execution block.
 if __name__ == '__main__':
@@ -29,40 +29,42 @@ if __name__ == '__main__':
     tList = np.array( [[i*dt for i in range( Nt )]] )
 
     # Set parameters.
-    epsList = [0.0, 5.0, 10.0];  Ne = len( epsList )
-    vcolor = ['mediumseagreen', 'mediumpurple', 'cornflowerblue']
+    epsList = [0.0];  Ne = len( epsList )
+    vcolor = ['cornflowerblue', 'mediumpurple']
     vlinestyle = Ne*['solid']
 
     # Initial vehicle positions.
     V0 = np.zeros( (2,1) )
 
     # Used for plotting without sim.
-    xList = np.nan*np.ones( (m,Nt,Nx) )
-    yList = np.nan*np.ones( (m,Nt,Nx) )
-    VList = np.nan*np.ones( (m,Nt,2) )
+    yList = np.nan*np.empty( (m,Nt,Nx) )
+    xList = np.nan*np.empty( (m,Nt,Nx) )
+    VList = np.nan*np.empty( (m,Nt,2) )
 
     # Initialize simulation variables.
     fig, axs = plt.subplots( 1,Ne+1 )
 
     # Simulation block.
     X0 = [None for i in range( Ne )]
+    Y0 = [None for i in range( Ne )]
     xswrm = [None for i in range( Ne )]
     yswrm = [None for i in range( Ne )]
     cand = [None for i in range( Ne )]
-    for i in range( Ne ):
+    for i, eps in enumerate( epsList ):
         X0[i] = Abound/2*np.hstack(
             [rotz(k*2*np.pi/m)@[[1],[0]] for k in range( m )]
             ) + noiseCirc( eps=delta, N=m )
+        Y0[i] = distanceBasedControl( X0[i], Xeq, C, K, B, A=Aset, eps=eps )[1]
 
         _, _, xswrm[i], _, _ = initEnvironment(
             fig, [axs[i], axs[-1]], X0[i], Xeq, Aset, V0, Nt=Nt)
-        yswrm[i] = Swarm2D( X0[i], fig=fig, axs=axs[i],
-            radius=-0.30, color='yellowgreen', tail_length=Nt,
-            draw_tail=sim ).setLineStyle( '--' ).draw()
+        # yswrm[i] = Swarm2D( X0[i], fig=fig, axs=axs[i],
+        #     radius=-0.30, color='yellowgreen', tail_length=Nt,
+        #     draw_tail=sim ).setLineStyle( '--' ).draw()
 
     # Simulation block.
     for i, eps in enumerate( epsList ):
-        X = X0[i];  Y = X0[i]
+        X = X0[i];  Y = Y0[i]
         V0 = lyapunovCandidatePerVehicle( m, 0, X0[i], Xeq )
 
         xList[:,0,:] = X.T
@@ -85,13 +87,13 @@ if __name__ == '__main__':
 
         plotEnvironment( fig, [axs[i], axs[-1]], xswrm[i], xList, VList,
             plotXf=False, vcolor=vcolor[i], linestyle=vlinestyle[i], linewidth=1.5 )
-        plotEnvironment( fig, [axs[i], axs[-1]], yswrm[i], yList,
-            plotXf=False, xcolor='yellowgreen', zorder=z_swrm-100 )
+        # plotEnvironment( fig, [axs[i], axs[-1]], yswrm[i], yList,
+        #     plotXf=False, xcolor='yellowgreen', zorder=z_swrm-100 )
 
     # Plot and axis labels.
-    titles = ['$\\varepsilon = %.1f$' % eps for eps in epsList] + ['Lyapunov Trend']
-    xlabels = [None, '$x$', None, 'Iteration']
-    ylabels = ['$y$', None, None, '$V(x)$']
+    titles = ['$\\theta=\\pi/4$', 'Lyapunov Trend']
+    xlabels = ['$x$', 'Iteration']
+    ylabels = ['$y$', '$V(x)$']
     for a, title, xlabel, ylabel in zip( axs, titles, xlabels, ylabels ):
         a.set_title( title )
         a.set_xlabel( xlabel )
@@ -101,24 +103,15 @@ if __name__ == '__main__':
     legend_elements_1 = [
         Line2D([0], [0], color='cornflowerblue', linestyle='none', marker='x',
             label='$X^{(0)}$'),
-        Line2D([0], [0], color='indianred', linestyle='none', marker='o', markeredgecolor='k',
-            label='$\\mathcal{A}$' ),
         Line2D([0], [0], color='cornflowerblue', markerfacecolor='none',
             label='$X$'),
-        Line2D([0], [0], color='yellowgreen',
-            label='$K(h(x) - b)$'),
+        Line2D([0], [0], color='indianred', linestyle='none', marker='o', markeredgecolor='k',
+            label='$\\mathcal{A}$' )
     ]
-    # axs[0].axis( Abound*np.array( [-1, 1, -1.1, 1.2] ) )
-    axs[0].legend( handles=legend_elements_1, fontsize=fontsize-2, ncol=2, loc=1 )
+    axs[1].legend( handles=legend_elements_1, fontsize=fontsize )
 
-    legend_elements_2 = [
-        Line2D([0], [0], color=vcolor[i], linestyle=vlinestyle[i], linewidth=2,
-            label='$\\varepsilon = %.1f$' % eps) for i, eps in enumerate( epsList )
-    ]
-    axs[-1].legend( handles=legend_elements_2, fontsize=fontsize-2, ncol=1 )
-
-    fig.set_figwidth( 2.5*plt.rcParams.get('figure.figsize')[0] )
-    fig.set_figheight( figheight )
+    fig.set_figwidth( plt.rcParams.get('figure.figsize')[0] )
+    fig.set_figheight( 3/4*figheight )
     fig.tight_layout()
     if show:
         plt.show( block=0 )
@@ -126,6 +119,6 @@ if __name__ == '__main__':
     # Calculate error after transformation.
     ans = input( 'Press ENTER to exit program... ' )
     if save or ans == 'save':
-        filename = 'single/homing.pdf'
+        filename = 'single/break.pdf'
         fig.savefig( figurepath + filename, dpi=600 )
         print( 'Figure saved to:\n ' + figurepath + filename )
